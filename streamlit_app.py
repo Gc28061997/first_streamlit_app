6cab93f90992436f07dce9c1ead952f977e75856
GargiChaurasia <117975906+Gc28061997@users.noreply.github.com>
2023-07-24 13:37:05 +0530

    Update streamlit_app.py
    add some lines with menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,8 @@ import streamlit
 streamlit.title('My Parent New healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale,Spinach & Rocket Smoothei')
+
+streamlit.text('Hard Boiled Free_Rang Egg')
+
 
